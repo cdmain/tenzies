1934a7d3eceb53667bc87fa0fa792fe7b9893668
cdhome <jordansewpershad@outlook.com>
2025-03-24 15:36:23 +0200

    completed styles for die component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,20 @@
+import Die from "./Die"
+
 export default function Main() {
     return (
         <main className="page">
+            <div className="die-container">
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
         </main>
     )
 }
