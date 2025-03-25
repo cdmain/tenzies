2747f37fe19f423410b75b7322b8b1f157390bc4
cdhome <jordansewpershad@outlook.com>
2025-03-25 12:13:00 +0200

    added roll button and changed font to karla

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react"
 import Die from "./Die"
 
 export default function Main() {
+
+    const [die, setDie] = useState(generateAllNewDice())
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+
+    const rollDice = die.map(num => (
+            <Die value={num} />
+        ))
+
+    function reRollDice() {
+        setDie(generateAllNewDice())
+    }
+
     return (
         <main className="page">
             <div className="die-container">
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
+                {rollDice}
             </div>
+            <button className="rollButton" onClick={reRollDice}>Roll</button>
         </main>
     )
 }
